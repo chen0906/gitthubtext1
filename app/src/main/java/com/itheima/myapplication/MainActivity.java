e31d5197f626653a5fe7fd323b6cb585604706cb
chen <chen>
2017-04-16 22:54:56 +0800

    布局文件中添加了button

--- app/src/main/java/com/itheima/myapplication/MainActivity.java
+++ app/src/main/java/com/itheima/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.itheima.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
     }
 }
