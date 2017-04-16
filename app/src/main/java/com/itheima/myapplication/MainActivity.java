084fa0308d8129fe4085a1fe83ccc5ad09d0d7f5
chen <chen>
2017-04-16 23:00:07 +0800

    布局文件中添加了recyclerview

--- app/src/main/java/com/itheima/myapplication/MainActivity.java
+++ app/src/main/java/com/itheima/myapplication/MainActivity.java
@@ -2,16 +2,19 @@ package com.itheima.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button button;
+    public RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
     }
 }
