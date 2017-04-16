b55265ae52c709165bd018ed0349fa47f6b47dcb
chen <chen>
2017-04-16 23:03:00 +0800

    布局文件中添加了listview

--- app/src/main/java/com/itheima/myapplication/MainActivity.java
+++ app/src/main/java/com/itheima/myapplication/MainActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button button;
     public RecyclerView recyclerView;
+    public ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        listView = (ListView) findViewById(R.id.listview);
     }
 }
